1451072927295d09c195f1557f42b405b1644faf
ItikiSatochi <toshiitiki@gmail.com>
2024-09-06 17:52:05 -0300

    new file:   If.py 	new file:   exercicio.ipynb

---
+++ If.py
@@ -0,0 +1,8 @@
+meta = 50000
+qtd_venda = 150000
+
+if qtd_venda >= meta:
+    print(f'Vendemos muito bem, o total é {float(qtd_venda)} unidade!')
+else:
+    print(f'Não conseguimos vender bem! Força e Continue.')
+
