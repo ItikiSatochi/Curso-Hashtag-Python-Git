017c40a9d0b5d431635db321978752fe597ecde7
ItikiSatochi <toshiitiki@gmail.com>
2024-09-10 19:34:09 -0300

    modified:   If.py

--- If.py
+++ If.py
@@ -6,3 +6,15 @@ if qtd_venda >= meta:
 else:
     print(f'Não conseguimos vender bem! Força e Continue.')
 
+meta = 20000
+vendas_realizadas = 25000
+
+
+if vendas_realizadas < meta:
+    print('não ganhou o bonus')
+elif vendas_realizadas > (meta * 2):
+    bonus = 0.07 * vendas_realizadas 
+    print(f'Ganhou {bonus} de bonus')
+else:
+    bonus = 0.03 * vendas_realizadas
+    print(f'ganhou {bonus} de bonus')
